6629f5dd71a27da999cb0e0a388d4c5f36323b41
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-26 00:30:50 +0530

    simple app using amqp.node client

--- recieve.js
+++ recieve.js
@@ -2,4 +2,18 @@
 // ˜˜˜˜∆˚¬√√∂∆∫ß∂ß∫µ ≤µçß˚∆ˆ¬∑∆˜˚≤µ´ ç˜ µ˜˜≥≤   ≤˜∑¨†ƒ©ˆ¨œ˙˚≥≤˜≤ µ≤µß∫
 
 
+var amqp = require('amqplib/callback_api');
 
+amqp.connect('amqp://localhost:5672', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var Q = 'Hello';
+
+    ch.assertQueue(Q, {durable: false});
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", Q);
+    ch.consume(Q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+
+  });
+});
--- send.js
+++ send.js
@@ -2,20 +2,30 @@
 
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', (err, conn) => {
+amqp.connect('amqp://localhost:5672', function(err, conn){
   conn.createChannel((err, ch) =>  {
 
-  	let message = 'Divyanshu';
+  	let Q = 'Hello';
+  	let message = "Hello World!";
 
-  	ch.assertQueue(message,{durable:false});
+  	ch.assertQueue(Q,{durable:false});
 
-  	ch.sendToQueus(message, new Buffer('Hey!'));
+  	ch.sendToQueue(Q, Buffer.from(message));
 
   	console.log('Message Sent!!');
 
+  	setTimeout(function() { conn.close(); process.exit(0) }, 500);
+
   });
+
+
+
 });
 
 // idempotent, clients can make that same call repeatedly while producing the same result.
 
 // To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+
+// Declaring a queue is idempotent - it will only be created if it doesn't exist already. The message content is a byte array, so you can encode whatever you like there.
+
+
